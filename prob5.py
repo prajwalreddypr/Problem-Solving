a40709c4ee7f5f1b886f53655ccdb46bd2556455
Prajwal Reddy <reddyprajwal2000@gmail.com>
2025-11-24 22:14:47 +0100

    updates

--- prob5.py
+++ prob5.py
@@ -7,3 +7,21 @@
 # Example:
 # Input: [1, 2, 2, 3, 1, 4, 2]
 # Output: [1, 2, 2, 1, 2]
+
+list = [1, 2, 2, 3, 1, 4, 2]
+
+dict_count = {}
+final_list = []
+
+for item in list:
+    if item in dict_count:
+        dict_count[item] += 1
+    else:
+        dict_count[item] = 1
+        
+
+for item in list:
+    if dict_count[item] > 1:
+        final_list.append(item)
+
+print(final_list)
