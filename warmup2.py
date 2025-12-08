03063d393c9442bafd7ca77d2c4071bf942d1bd1
Prajwal Reddy <reddyprajwal2000@gmail.com>
2025-12-09 00:59:42 +0100

    updates

--- warmup2.py
+++ warmup2.py
@@ -2,3 +2,72 @@
 # (Strings, lists, loops and basic logic)
 
 #CONCATENATE 2 STRINGS
+# str1 = "Hello"
+# str2 = "World"
+# result = f"{str1} {str2}"
+# print(result)
+
+
+#REPEAT A STRING 3 TIMES
+# text = "hui"
+# print(text * 3)
+
+
+#STRING STARTS WITH A SUBSTRING
+# text = "Python is lovely"
+# print(text.startswith("Py"))
+# print(text.startswith("Yt"))
+# print(text.endswith("ly"))
+# print(text.endswith("fsd"))
+
+
+
+
+#index of a character in a string
+# str = "ParisIsGreat"
+# print(str.index("I"))
+
+
+#NO of words in a string
+# str = "This is the season of Christmas"
+# words = str.split()
+# print(len(words))
+
+
+
+#REVERSE A LIST
+# list = [1,2,3,4,5]
+# list.reverse()
+# print(list)
+
+
+
+#SORTING A LIST OF NUMBERS
+# numbers = [2,43,5,4,5,3,2,5,56]
+# print(numbers)
+# numbers.sort()
+# print(numbers)
+
+
+
+
+#SORTING LIST OF STRINGS ALPHABETICALLY
+# strs = ["Alice", "Charlie", "Bob"]
+# strs.sort()
+# print(strs)
+
+
+
+#REVERSE A STRING USING A LOOP
+# str = "Paris and Python"
+# rev = ""
+
+# for char in str:
+#     rev = char + rev
+
+# print(rev)
+
+
+
+
+#REMOVE DUPLICATES FROM LIST
