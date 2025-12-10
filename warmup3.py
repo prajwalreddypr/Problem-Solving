fb0f029ab65e52ca612f48fd99b33df5073f81e3
Prajwal Reddy <reddyprajwal2000@gmail.com>
2025-12-10 09:57:58 +0100

    updates

---
+++ warmup3.py
@@ -0,0 +1,24 @@
+#FIRST NON REPEATING CHARACTER INNA STRING
+
+# def first_non_repeat(str):
+#     count = {}
+    
+#     for char in str:
+#         if char in count:
+#             count[char] += 1
+#         else:
+#             count[char] = 1
+    
+#     for char in str:
+#         if count[char] == 1:
+#             return char
+    
+#     return None
+
+# print(first_non_repeat("aaaabccccd"))
+
+
+
+
+#rotate a List by K positions
+
